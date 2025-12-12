1eb2b94bd19c20ef85fd17cab79dc812344d0303
Saadan77 <saadanjawad50@gmail.com>
2025-12-13 02:44:50 +0500

    Uncomment Brand, Partners, CTA, and Footer components in Home function

--- src/app/page.js
+++ src/app/page.js
@@ -33,10 +33,10 @@ export default function Home() {
       <About2 />
       <Proficients />
       <Portfolio />
-      {/* <Brand />
+      <Brand />
       <Partners />
       <CTA />
-      <Footer /> */}
+      <Footer />
     </>
   );
 }
